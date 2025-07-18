f7f24b374bf9dcaa5180fd7720dfd51f8738e9ac
Margaret Hansen <margareh@andrew.cmu.edu>
2025-07-18 15:07:51 -0700

    Setup for local build

--- setup_conda.py
+++ setup_conda.py
@@ -1,8 +1,6 @@
-# Description: Setup file for 'manual' installation (this method works for
-#              instance with 'pip')
+# Description: Setup file
 #
-# Installation of package: mv setup_manual.py setup.py
-#                          python -m pip install .
+# Installation of package: python -m pip install .
 #
 # Copyright (c) 2022 ETH Zurich, Christian R. Steger
 # MIT License
@@ -15,49 +13,38 @@ from Cython.Distutils import build_ext
 import numpy as np
 
 # -----------------------------------------------------------------------------
-# Manual settings
-# -----------------------------------------------------------------------------
-
-# Paths for Intel Embree and Threading Building Blocks (TBB)
-path_include = ["/opt/local/include/"]
-path_lib = ["/opt/local/lib/libembree3"]  # without file ending
-# - depending on defined library paths and loaded modules, it might be
-#   necessary to add paths to further libraries like 'libimf' and 'libtbb'
-# - in case a library is not found during execution of HORAYZON, it has to be
-#   defined before running Python/HORAYZON via 'LD_LIBRARY_PATH'.
-
-# Compiler
-compiler = "clang++"  # (like gcc, clang, clang++, default)
-# "default": compiler is not redefined
-
-# -----------------------------------------------------------------------------
 # Operating system dependent settings
 # -----------------------------------------------------------------------------
 
+path_lib_conda = os.environ["CONDA_PREFIX"] + "/lib/"
 if sys.platform in ["linux", "linux2"]:
     print("Operating system: Linux")
     lib_end = ".so"
+    compiler = "gcc"
+    extra_compile_args_cython = ["-O3", "-ffast-math"]
     extra_compile_args_cpp = ["-O3"]
 elif sys.platform in ["darwin"]:
     print("Operating system: Mac OS X")
     lib_end = ".dylib"
+    compiler = "clang"
+    extra_compile_args_cython = ["-O3", "-ffast-math",
+                                 "-Wl,-rpath," + path_lib_conda,
+                                 "-L" + path_lib_conda]
     extra_compile_args_cpp = ["-O3", "-std=c++11"]
 elif sys.platform in ["win32"]:
     print("Operating system: Windows")
     print("Warning: Package not yet tested for Windows")
 else:
     raise ValueError("Unsupported operating system")
-extra_compile_args_cython = ["-O3", "-ffast-math"]
 libraries_cython = ["m", "pthread"]
-include_dirs_cpp = [np.get_include()] + path_include
-extra_objects_cpp = [i + lib_end for i in path_lib]
+include_dirs_cpp = [np.get_include()]
+extra_objects_cpp = [path_lib_conda + i + lib_end for i in ["libembree3"]]
 
 # -----------------------------------------------------------------------------
 # Compile Cython/C++ code
 # -----------------------------------------------------------------------------
 
-if compiler != "default":
-    os.environ["CC"] = compiler
+os.environ["CC"] = compiler
 
 ext_modules = [
     Extension("horayzon.transform",
